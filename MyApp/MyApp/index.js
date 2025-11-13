c498ea562c0caf7aca917d8be2608be905eeb297
chatme291 <49653790-chatme291@users.noreply.replit.com>
2025-11-13 07:10:14 +0000

    update  file

--- MyApp/MyApp/index.js
+++
@@ -1,4 +0,0 @@
-import { registerRootComponent } from 'expo';
-import App from './App'; // ⬅️ pastikan ini tanpa /src
-
-registerRootComponent(App);
--- MyApp/MyApp/src/screen/party/tabs/AsiaPartyTab.js
+++
@@ -1,3 +0,0 @@
-import React from "react";
-import HotPartyTab from "./HotPartyTab";
-export default HotPartyTab;
